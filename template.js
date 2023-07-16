78fbcb047d5dc914a8c4ae4bc7cd52faf53a4385
hamitmizrak <hamitmizrak@gmail.com>
2023-07-16 18:34:48 +0300

    dark mode

--- template.js
+++ template.js
@@ -1,7 +1,14 @@
 
+// Dark Mode
+let darkLight=()=>{
+    let elementData=document.body.classList.toggle("dark_mode_class");
+}
+
+// Search
+
 // now date
-const nowDate=()=>{
-let dateYear=document.getElementById("now_date");
-dateYear.innerHTML=new Date().getFullYear();
+const nowDate = () => {
+    let dateYear = document.getElementById("now_date");
+    dateYear.innerHTML = new Date().getFullYear();
 }
 nowDate();
